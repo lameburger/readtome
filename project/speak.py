e173875540b093259650d0460123f26205a92125
markhoehn <hoehn404@gmail.com>
2022-11-12 02:29:08 -0600

    ll

---
+++ project/speak.py
@@ -0,0 +1,16 @@
+# tts library
+import pyttsx3
+import tkinter 
+# declare speaker object
+speaker = pyttsx3.init()
+#Set speed 
+speaker.setProperty('rate', input("rate: "))
+# Set volume 0-1
+speaker.setProperty('volume', input("volume from 0-1: "))
+# Set text
+with open('page.txt') as f:
+    text = f.read().replace('\n', '')
+#Speak
+speaker.say(text)
+speaker.runAndWait()
+#
