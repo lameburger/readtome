e5293a0a86508bc428337986736e1b98719e94aa
markhoehn <hoehn404@gmail.com>
2022-11-12 02:22:37 -0600

    speak.py now reads from page.txt

--- project/speak.py
+++ project/speak.py
@@ -8,10 +8,8 @@ speaker.setProperty('rate', input("rate: "))
 # Set volume 0-1
 speaker.setProperty('volume', input("volume from 0-1: "))
 # Set text
-text = input("text: ")
-# Set voice with path ex: HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0"
-speaker.setProperty('voice', input("path: ")
+with open('page.txt') as f:
+    text = f.read().replace('\n', '')
 #Speak
 speaker.say(text)
 speaker.runAndWait()
-# 
